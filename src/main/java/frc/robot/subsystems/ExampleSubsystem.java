ac2f7242409cc2b65a91902c1f00742323adad80
The DiscoTechs - Team 1099 <36282374+DiscoTechs@users.noreply.github.com>
2024-11-20 20:24:07 -0500

    Sync for Branch Switch

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,17 +4,27 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Command;
+
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 import frc.robot.Constants;
 
 public class ExampleSubsystem extends SubsystemBase {
-  private CANSparkMax motor = new CANSparkMax(Constants.MotorConstants.Motor, MotorType.kBrushless);
+  private VictorSPX motor = new VictorSPX(41);
+  private DigitalInput limit = new DigitalInput(2);
   private boolean inverted = false;
   private double speed = 0.0;
+
+
+
+
+
 
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
@@ -30,9 +40,18 @@ public class ExampleSubsystem extends SubsystemBase {
     });
   }
 
-  public Command speedUp() { return runOnce(() -> speed += 0.05); }
-  public Command slowDown() { return runOnce(() -> speed -= 0.05); }
-  public Command setSpeed(double s) { return runOnce(() -> speed = s); }
+  public Command speedUp() { 
+    return runOnce(() -> speed += 0.05); 
+  }
+
+  public Command slowDown() { 
+    return runOnce(() -> speed -= 0.05); 
+  }
+
+  public Command setSpeed(double s) { 
+    return runOnce(() -> speed = s); 
+  }
+
   public Command invertMotor() {
     return runOnce(() -> {
       inverted = !inverted;
@@ -51,7 +70,7 @@ public class ExampleSubsystem extends SubsystemBase {
   }
 
   public double getEncoderValue() {
-    return motor.getEncoder().getPosition();
+    return 0; //motor.getEncoder().getPosition();
   }
 
   @Override
@@ -59,13 +78,26 @@ public class ExampleSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
 
     SmartDashboard.putNumber("Speed", speed);
-    SmartDashboard.putNumber("Encoder Absolute", this.getEncoderValue());
+    //SmartDashboard.putNumber("Encoder Absolute", this.getEncoderValue());
+    SmartDashboard.putBoolean("Limit", limit.get());
+    motor.set(VictorSPXControlMode.PercentOutput, speed);
 
-    motor.set(speed);
+    if (limit.get() == false){
+      motor.set(VictorSPXControlMode.PercentOutput, 0);
+    }
+
+    else{
+      motor.set(VictorSPXControlMode.PercentOutput, speed);
+    }
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
+  public boolean limitInfo(){
+    return limit.get();
+  }
+
 }
